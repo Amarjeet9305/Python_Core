73f3fe40046bea783b5f24d50266efc6647eb5df
amarjeet930535 <amarjeet930535@gmail.com>
2025-10-11 14:58:05 +0530

    python file

--- pattern/pyrmid_pattern.py
+++ pattern/pyrmid_pattern.py
@@ -1,8 +1,11 @@
 def pyrmid_patter(n):
     for i in range(n):
-        print("e",end="")
-    for j in range(n-1,i):
-        print("",end="")
+        # Print space
+        for j in range(n-i-1):
+            print(" ",end="")
+        # Print Stars
+        for j in range(2*i+1):
+            print("*",end="")
+        print()        
 # Driver code
 pyrmid_patter(3)           
-    
